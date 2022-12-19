c4c28cd2e4730d9cdc712b4601d30c3ec9cf4b05
Irene Aguas <iaguas@quantifycrypto.net>
2022-12-18 18:23:18 -0800

    ready for deployment

---
+++ src/components/Product.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card } from 'react-bootstrap';
+import Rating from './Rating';
+
+const Product = ({ product }) => {
+  return (
+    <Card className='mb-2 mx-3 mb-5 shadow '>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} className='shadow' />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+        <Card.Text as='div'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+        <Card.Text as='h3'>${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
